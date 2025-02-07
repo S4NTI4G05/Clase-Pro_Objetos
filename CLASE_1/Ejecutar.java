1f9923199b70b5e6d01410ca7c836f5d56e8db0f
S4NTI4G05 <davids.moreno@campusucc.edu.co>
2025-02-07 01:01:50 +0000

    Se agrego la palabra Java

--- CLASE_1/Ejecutar.java
+++ CLASE_1/Ejecutar.java
@@ -1,7 +1,6 @@
 public class Ejecutar {
-
     public static void main(String[] args) {
-        System.out.println("¡HOLA MUNDO!");
+        System.out.println("¡HOLA MUNDO! JAVA");
         
     }
 }
