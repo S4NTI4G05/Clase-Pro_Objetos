479825d22c2819ff756bed0f334a19d07322027a
S4NTI4G05 <davids.moreno@campusucc.edu.co>
2025-02-14 01:22:30 +0000

    Ejercicios Clase 2

---
+++ Clase_2/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    //Metodo mein sirve para arrancar
+    public static void main(String[] args) {
+        //Tipos de datos 
+
+        //Enteros int, long
+        int edad = 19;
+
+        //Reales double, float
+        double altura = 1.77;
+
+        //Cadenas
+        String nombre = "Santiago";
+
+        //Logicos
+        boolean esEstudiante = true; //false
+
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad + ",  Altura: " + altura + ", Estudiante: " + esEstudiante + "");
+    }
+}
