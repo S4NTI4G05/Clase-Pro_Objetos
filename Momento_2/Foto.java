6fbbaebc6a1c1a25005a097ed6b41d181aea5bbd
S4NTI4G05 <davids.moreno@campusucc.edu.co>
2025-04-09 23:03:29 -0500

    Crreacion de la clase Foto, con sus respectivos metodos y su constructor

--- Momento_2/Foto.java
+++ Momento_2/Foto.java
@@ -2,7 +2,46 @@ package Momento_2;
 
 public class Foto {
     private String fichero;
-    public void print(){
+    private String fotoColor;
+    private String fotoBlancoNegro;
+    private int nuemeoCopias;
 
+    ///Constructor
+    public Foto(String fichero, String fotoColor, String fotoBlancoNegro, int nuemeoCopias) {
+        this.fichero = fichero;
+        this.fotoColor = fotoColor;
+        this.fotoBlancoNegro = fotoBlancoNegro;
+        this.nuemeoCopias = nuemeoCopias;
     }
+
+    //Metodos
+    public void setFichero(String fichero) {
+        this.fichero = fichero;
+    }
+    public String getFichero() {
+        return fichero;
+    }
+    public void setFotoColor(String fotoColor) {
+        this.fotoColor = fotoColor;
+    }
+    public String getFotoColor() {
+        return fotoColor;
+    }
+    public void setFotoBlancoNegro(String fotoBlancoNegro) {
+        this.fotoBlancoNegro = fotoBlancoNegro;
+    }
+    public String getFotoBlancoNegro() {
+        return fotoBlancoNegro;
+    }
+    public void setNuemeoCopias(int nuemeoCopias) {
+        this.nuemeoCopias = nuemeoCopias;
+    }
+    public int getNuemeoCopias() {
+        return nuemeoCopias;
+    }
+    public String toString() {
+        return "Foto{" + "fichero=" + fichero + ", fotoColor=" + fotoColor + ", fotoBlancoNegro=" + fotoBlancoNegro + ", nuemeoCopias=" + nuemeoCopias + '}';
+    }
+
 }
+
